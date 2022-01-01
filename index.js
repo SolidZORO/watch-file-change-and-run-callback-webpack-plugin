1b7cf6652e7cb4bdfcb0c6506d278ad616c1c029
Jason Feng <solidzoro@live.com>
2022-01-01 22:22:06 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,26 @@
+class WatchFileChangeAndRunCallbackWebpackPlugin {
+  constructor(opts) {
+    this.opts = opts;
+  }
+
+  apply(compiler) {
+    compiler.hooks.watchRun.tap(
+      'WatchFileChangeAndRunCallbackWebpackPlugin',
+      (comp) => {
+        if (!comp.modifiedFiles) return;
+
+        // ONLY One
+        const changedFile = [...comp.modifiedFiles][0];
+        if (!changedFile) return;
+
+        this.opts.matchs.forEach((m) => {
+          if (!m.filePath || m.filePath !== changedFile) return;
+
+          if (m.callback && typeof m.callback === 'function') m.callback();
+        });
+      },
+    );
+  }
+}
+
+module.exports = WatchFileChangeAndRunCallbackWebpackPlugin;
